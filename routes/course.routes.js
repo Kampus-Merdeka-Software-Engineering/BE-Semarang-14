09063547a21f654d73c9fc955f9c7ca165f61d9f
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-09-30 03:43:28 +0700

    rename peserta routes

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -11,7 +11,7 @@ const pesertaController = require('../controllers/peserta.controllers');
 // define routes
 router.get('/courses', courseController.getAllCourses); // get all courses
 router.get('/courses/:id', courseController.getCourseById); // get course by id
-router.post('/course/peserta', pesertaController.postPeserta); // post peserta
+router.post('/courses', pesertaController.postPeserta); // post peserta
 
 // export router
 module.exports = router;
