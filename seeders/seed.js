7e6a42d2851be12b4b4a1d9768ee5f14b4770657
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-09-30 04:24:47 +0700

    update seeder

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,13 @@
+// import sequelize
+const sequelize = require('../config/db_config');
+
 // import model
 const Course = require('../model/course.model');
+const Peserta = require('../model/peserta.model');
 
-// seed data course
-(async () => {
-    await Course.create({
+// Seeder data Course
+const courseSeed = [
+    {
         id: 'C001',
         nama: 'UI/UX Design',
         deskripsi_singkat: 'UI/UX Design dapat membantu peserta mempersiapkan diri untuk berkarir di industri desain digital yang terus berkembang dan memberikan pemahaman yang mendalam tentang bagaimana menciptakan pengalaman pengguna yang luar biasa di dunia digital.',
@@ -11,8 +15,8 @@ const Course = require('../model/course.model');
         rating: 9.8,
         harga: 4000000,
         photo: '',
-    });
-    await Course.create({
+    },
+    {
         id: 'C002',
         nama: 'Software Engineering',
         deskripsi_singkat: 'Kejar salah satu karir paling tangguh di bidang Teknologi sebagai Insinyur Perangkat Lunak dengan pemahaman komprehensif tentang pengembangan web front-end hingga back-end.',
@@ -20,8 +24,8 @@ const Course = require('../model/course.model');
         rating: 9.9,
         harga: 5000000,
         photo: '',
-    });
-    await Course.create({
+    },
+    {
         id: 'C003',
         nama: 'Data Analyst',
         deskripsi_singkat: 'Gunakan keahlian analisis data, pola pikir bisnis, dan keterampilan komunikasi Anda serta jawab pertanyaan bisnis yang kompleks dengan data.',
@@ -29,5 +33,20 @@ const Course = require('../model/course.model');
         rating: 9.8,
         harga: 2000000,
         photo: '',
+    }
+];
+
+// Sync model to database
+sequelize.sync()
+    .then(async () => {
+        // Course Seeder
+        await Course.destroy({ where: {} }); // delete all existing rows in the "Course" table
+        await Course.bulkCreate(courseSeeds); // seed new course data to database
+
+        // End Process
+        process.exit(0); // Keluar dari proses setelah selesai
+    })
+    .catch((err) => {
+        console.error('Error:', err);
+        process.exit(1); // Keluar dengan kode kesalahan
     });
-})();
