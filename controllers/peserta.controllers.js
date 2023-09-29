0cf48c293828890f2625e4ddcafb5aa4eb54dfde
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-09-30 03:32:07 +0700

    create peserta model and controller

--- controllers/peserta.controllers.js
+++ controllers/peserta.controllers.js
@@ -1,2 +1,29 @@
-// import db model
-const db = require('../db');
+// import db config
+const sequelize = require('../config/db_config');
+
+// import model
+const Peserta = require('../model/peserta.model');
+
+// post peserta
+exports.postPeserta = async (req, res) => {
+    try {
+        // get data from body
+        const { id_course, nama, email, no_hp } = req.body;
+
+        // insert data
+        const peserta = await Peserta.create({
+            id_course,
+            nama,
+            email,
+            no_hp,
+        });
+
+        // send response
+        res.status(200).send({
+            message: 'success',
+            data: peserta,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
---
+++ model/peserta.model.js
@@ -0,0 +1,47 @@
+// import sequelize
+const { Sequelize, DataTypes } = require('sequelize');
+
+// import db config
+const sequelize = require('../config/db_config');
+
+// import model
+const Course = require('../model/course.model');
+
+// create model
+const Peserta = sequelize.define(
+    'peserta',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        id_course: {
+            type: DataTypes.STRING,
+            references: {
+                model: Course,
+                key: 'id',
+            },
+        },
+        nama: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        no_hp: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        timestamps: false,
+    }
+);
+
+// create table if not exists
+(async () => {
+    await sequelize.sync();
+})();
+
+// export model
+module.exports = Peserta;
